3a1eb9dd23f76ba608ac2bc40d712250a9731c65
Spencer Zeng <spencer_zeng@sfu.ca>
2021-02-21 14:30:26 -0800

    add error tests to demonstrate request structure

--- tests/mainTests.py
+++ tests/mainTests.py
@@ -4,16 +4,39 @@ from src.main import Query
 
 
 class MainTests(unittest.TestCase):
-    def test_QueryingEndpointHello_ReturnsHelloWorld(self):
-        schema = Schema(query=Query)
+    @classmethod
+    def setUpClass(cls):
+        cls._schema = Schema(query=Query)
+
+    def test_QueryHelloField_ReturnsHelloWorld(self):
         request = """{
             hello
         }"""
 
-        response = schema.execute(request)
+        response = self._schema.execute(request)
 
         self.assertIsNone(response.errors)
         self.assertEqual("Hello World!", response.data['hello'])
+
+    def test_QueryNotHelloField_ReturnsError(self):
+        request = """{
+            notHello
+        }"""
+
+        response = self._schema.execute(request)
+
+        # Since no schema with field "notHello" is defined on the server
+        self.assertIsNotNone(response.errors)
+
+    def test_QueryHelloWithArg_ReturnsError(self):
+        request = """{
+            hello(name: "spencer")
+        }"""
+
+        response = self._schema.execute(request)
+
+        # Since the schema with field "hello" does not have parameters
+        self.assertIsNotNone(response.errors)
 
 
 if __name__ == '__main__':
